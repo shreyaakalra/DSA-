9a5010f4d301cf219b5972c983c39ccb259c90ed
shreyaakalra <shreyaa.kalra@gmail.com>
2025-06-17 01:24:32 +0530

    tree structure basics

---
+++ treesbasicstructure.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// struct represents one node in a binary tree
+struct Node{
+  int data; // stores value in the node
+  struct Node *left; // pointer to the left child
+  struct Node *right; // pointer to the right child
+  
+  // this is a constructor
+  Node(int value){
+    data = value;
+    right = left = nullptr;
+  }
+
+};
+
+int main(){
+  struct Node *root = new Node(1);
+  root -> left = new Node(2);
+  root -> right = new Node(3);
+  root -> left -> right = new Node(5);
+  
+}
+
+
